f39f8ea1a5346f492b525ad70710b7c383df8262
YunSeoLee <yunseolee.dev@gmail.com>
2024-05-12 17:30:18 +0100

    Finish lab06

--- lab06/lib/MemoryStream.cpp
+++ lab06/lib/MemoryStream.cpp
@@ -28,7 +28,35 @@ void OutputMemoryStream::Write(const void* outData, size_t outByteCount)
 void OutputMemoryStream::WriteBits(uint8_t outData, size_t outBitCount)
 {
 	// TODO
-	
+	uint16_t resultBitHead = mBitHead + outBitCount;
+	uint32_t resultHead = mHead;
+	if(resultBitHead > 7){
+		resultBitHead = resultBitHead%8;
+		resultHead++;
+		if (resultHead > mCapacity)
+		{
+			ReallocBuffer(std::max(mCapacity * 2, resultHead));
+		}
+
+		uint8_t first_seg_len = 8-mBitHead;
+		uint8_t second_seg_len = outBitCount-first_seg_len;
+		uint8_t mask = (1 << first_seg_len)-1;
+		uint8_t write_data = (outData & mask) << mBitHead;
+		*(mBuffer+mHead) = ((*(mBuffer+mHead)) | write_data);
+
+		write_data = (outData & ~mask) >> first_seg_len;
+		*(mBuffer+resultHead) = write_data;
+		printf("write_data : %u\n",write_data);
+	}
+	else{
+		uint8_t write_data = (outData << mBitHead);
+		*(mBuffer+mHead) = ((*(mBuffer+mHead)) | write_data);
+		printf("write_data : %u\n",write_data);
+	}
+	printf("1st: %d\n",*(mBuffer+mHead));
+	mBitHead = (uint8_t)resultBitHead;
+	mHead = resultHead;
+	printf("2nd: %d\n",*(mBuffer+mHead));
 }	
 
 void InputMemoryStream::Read(void* inData,
@@ -51,5 +79,28 @@ void InputMemoryStream::Read(void* inData,
 void InputMemoryStream::ReadBits(uint8_t& inData, size_t inBitCount)
 {
 	// TODO
-	
+	uint16_t resultBitHead = mBitHead + inBitCount;
+	uint32_t resultHead = mHead;
+	if(resultBitHead > 7){
+		resultBitHead = resultBitHead%8;
+		resultHead++;
+
+		uint8_t first_seg_len = 8-mBitHead;
+		uint8_t second_seg_len = inBitCount-first_seg_len;
+		uint8_t read_data = (*(mBuffer+mHead) & ~((1 << mBitHead)-1)) >> mBitHead;
+		read_data = ((*(mBuffer+resultHead) & ((1<<second_seg_len)-1)) << first_seg_len) | read_data;
+		inData = read_data;
+	}
+	else{
+		uint8_t mask = ((1 << (mBitHead + inBitCount))-1) & ~((1 << mBitHead)-1);
+		uint8_t read_data = (*(mBuffer+mHead) & mask) >> mBitHead;
+		inData = read_data;
+	}
+	printf("resultBitHead: %d\n",resultBitHead);
+	printf("resultHead: %d\n",resultHead);
+	printf("1st: %d\n",*(mBuffer+mHead));
+	mBitHead = (uint8_t)resultBitHead;
+	mHead = resultHead;
+	printf("2nd: %d\n",*(mBuffer+mHead));
+	printf("read_data : %u\n", inData);
 }
